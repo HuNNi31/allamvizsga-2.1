41c1ad55ebe8b71587993ce77629767cead75628
HuNNi31 <csegzi_huni@yahoo.com>
2022-06-02 09:43:24 +0300

    javitva

--- MVCLogin/ASPX/PieChart.aspx.cs
+++ MVCLogin/ASPX/PieChart.aspx.cs
@@ -8,31 +8,36 @@ using System.Data.SqlClient;
 using System.Data;
 using System.Web.UI.DataVisualization.Charting;
 
-public partial class Default : System.Web.UI.Page
+namespace MVCLogin.ASPX
 {
-    protected void Page_Load(object sender, EventArgs e)
+    public partial class PieChart : System.Web.UI.Page
     {
-    }
-    void LoadData()
-    {
-        DataTable dt = new DataTable();
-        using (SqlConnection con = new SqlConnection(@"Data Source=.\sqlexpress;Initial Catalog-MyFirstTestDB;Integrated Security=True"))
+        protected void Page_Load(object sender, EventArgs e)
         {
-            con.Open();
-            SqlCommand cmd = new SqlCommand("select Tanzsek, Gradul_de_multumire as Rate from Adattipus group by Tanszek", con);
-            SqlDataAdapter da = new SqlDataAdapter();
-            da.Fill(dt);
-            con.Close();
         }
-        string[] x = new string[dt.Rows.Count];
-        int[] y = new int[dt.Rows.Count];
+        void LoadData()
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection con = new SqlConnection(@"Data Source=(local);Integrated Security=true;Initial Catalog=minosegbiztositas"))
+            {
+                con.Open();
 
-        for (int i = 0; i < dt.Rows.Count; i++)
-        {
-            x[i] = dt.Rows[i][0].ToString();
-            y[i] = Convert.ToInt32(dt.Rows[i][1]);
+                SqlCommand cmd = new SqlCommand("select Type, sum(Amount) as TotAmount from MyFirstTestDB group by Type", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                con.Close();
+            }
+            string[] x = new string[dt.Rows.Count];
+            int[] y = new int[dt.Rows.Count];
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                x[i] = dt.Rows[i][0].ToString();
+                y[i] = Convert.ToInt32(dt.Rows[i][1]);
+            }
+            Chart1.Series[0].Points.DataBindXY(x, y);
+            Chart1.Series[0].ChartType = SeriesChartType.Pie;
+           
         }
-        Chart1.Series[0].Points.DataBindXY(x, y);
-        Chart1sss.Series[0].ChartType = SeriesChartType.Pie;
     }
 }
