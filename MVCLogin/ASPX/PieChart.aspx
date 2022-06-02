ea9441d11adcd8f00c5b9eab901e791b2f19f057
HuNNi31 <csegzi_huni@yahoo.com>
2022-06-02 11:17:25 +0300

    üres_chart

--- MVCLogin/ASPX/PieChart.aspx
+++ MVCLogin/ASPX/PieChart.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PieChart.aspx.cs" Inherits="MVCLogin.ASPX.PieChart" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PieChart.aspx.cs" Inherits="MVCLogin.ASPX.PieChart"  %>
 
 <%@ Register assembly="System.Web.DataVisualization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" namespace="System.Web.UI.DataVisualization.Charting" tagprefix="asp" %>
 
@@ -6,24 +6,33 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>PieChart</title>
 </head>
 <body>
     <form id="form1" runat="server">
         <div>
 
-            <asp:Chart ID="Chart1" runat="server" BackImageAlignment="Center">
-                <series>
-                    <asp:Series Name="Series1">
+            <asp:Chart ID="Chart1" runat="server" DataSourceID="SqlDataSourcePie" Width="283px">
+                <Series>
+                    <asp:Series Name="Series1" ChartType="Pie" XValueMember="Tanszek" YValueMembers="Szint">
                     </asp:Series>
-                </series>
-                <chartareas>
+                </Series>
+                <ChartAreas>
                     <asp:ChartArea Name="ChartArea1">
                     </asp:ChartArea>
-                </chartareas>
+                </ChartAreas>
             </asp:Chart>
 
+            <asp:SqlDataSource ID="SqlDataSourcePie" runat="server" ConnectionString="<%$ ConnectionStrings:minosegbiztositasConnectionStringPie %>" SelectCommand="SELECT Tanszek, AVG(Gradul_de_multumire) AS Szint FROM Adattipus GROUP BY Tanszek"></asp:SqlDataSource>
+ </div>
+        <div>
+            <asp:GridView ID="GridView1" runat="server">
+                <RowStyle ForeColor="Black" />
+            </asp:GridView>
         </div>
+
+
+        
     </form>
 </body>
 </html>
--- MVCLogin/ASPX/PieChart.aspx.cs
+++ MVCLogin/ASPX/PieChart.aspx.cs
@@ -14,30 +14,41 @@ namespace MVCLogin.ASPX
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                LoadData();
+            }
         }
         void LoadData()
         {
             DataTable dt = new DataTable();
+            //using (SqlConnection con = new SqlConnection(@"Data Source=.\sqlexpress;Initial Catalog-minosegbiztositas;Integrated Security=True"))
             using (SqlConnection con = new SqlConnection(@"Data Source=(local);Integrated Security=true;Initial Catalog=minosegbiztositas"))
             {
                 con.Open();
 
-                SqlCommand cmd = new SqlCommand("select Type, sum(Amount) as TotAmount from MyFirstTestDB group by Type", con);
+                SqlCommand cmd = new SqlCommand("select Tanszek AS Tanszék, AVG(Gradul_de_multumire) AS Értékelés from Adattipus group by Tanszek", con);
                 SqlDataAdapter da = new SqlDataAdapter(cmd);
                 da.Fill(dt);
-                con.Close();
+                //con.Close();
             }
-            string[] x = new string[dt.Rows.Count];
-            int[] y = new int[dt.Rows.Count];
+            //string[] x = new string[dt.Rows.Count];
+            //int[] y = new int[dt.Rows.Count];
 
-            for (int i = 0; i < dt.Rows.Count; i++)
-            {
-                x[i] = dt.Rows[i][0].ToString();
-                y[i] = Convert.ToInt32(dt.Rows[i][1]);
-            }
-            Chart1.Series[0].Points.DataBindXY(x, y);
-            Chart1.Series[0].ChartType = SeriesChartType.Pie;
-           
+            //for (int i = 0; i < dt.Rows.Count; i++)
+            //{
+            //    x[i] = dt.Rows[i][0].ToString();
+            //    y[i] = Convert.ToInt32(dt.Rows[i][1]);
+            //}
+            //Chart1.DataSource = dt;
+            Chart1.Series["Series1"].XValueMember = "Tanszek";
+            Chart1.Series["Series1"].YValueMembers = "Szint";
+            Chart1.Series["Series1"].ChartType = SeriesChartType.Pie;
+            Chart1.ChartAreas["ChartArea1"].Area3DStyle.Enable3D = true;
+           // Chart1.Legends["Series1"].Enabled = true;
+
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
         }
     }
 }
